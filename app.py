9feaf9900fa546bb38c0f62e80c3ec14787b7530
ReylordDev <98845999+ReylordDev@users.noreply.github.com>
2024-03-21 15:55:45 +0100

    set up file upload functionality

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
+    if request.method == "POST":
+        print("POST")
+        if "file" not in request.files:
+            return "No file uploaded", 400
+        file = request.files["file"]
+        if file.filename == "":
+            return "No file selected", 400
+        if file:
+            file.save(f"uploads/{file.filename}")
+            return "File uploaded", 200
+
     return render_template("index.html")
 
 
---
+++ static/src/index.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  const dropZone = document.getElementById('dropZone');
+  const fileInput = document.getElementById('fileInput');
+  const uploadForm = document.getElementById('uploadForm');
+
+  dropZone.addEventListener("click", function () {
+    fileInput.click();
+  });
+
+  fileInput.addEventListener("change", function () {
+    if (fileInput.files.length > 0) {
+      uploadForm.submit();
+    }
+  });
+
+  dropZone.addEventListener("dragover", function (e) {
+    this.classList.add("dragover");
+  });
+
+  dropZone.addEventListener("dragleave", function (e) {
+    this.classList.remove("dragover");
+  });
+
+  dropZone.addEventListener("drop", function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.classList.remove("dragover");
+
+    let file = e.dataTransfer.files[0];
+    fileInput.files = e.dataTransfer.files;
+    uploadForm.submit();
+  });
+});
